3e9c66b31e0f8b5842e1e5ebee5c38a73ad6ff4f
domivika <72144341+domivika@users.noreply.github.com>
2021-04-21 01:30:50 +0200

    Update general_short_script.sh

--- general_short_script.sh
+++ general_short_script.sh
@@ -1,3 +1,5 @@
+### 4 bam files in: III20_proc; III21_proc; IV33_proc; V14_proc
+
 ### CNVnator
 # Docker
 docker run -it -v /home/dominika_kresa/mounted_from_Second/nie_pacz:/work/my_data -v /home/dominika_kresa/reference/:/work/reference -v /home/dominika_kresa/mounted_from_Second/clubfoot_DK:/work cnvnator
@@ -7,3 +9,8 @@ bash cnvnator_script.sh
 # Docker
 docker run -it -v /home/dominika_kresa/mounted_from_Second/nie_pacz:/work/my_data -v /home/dominika_kresa/reference/:/work/reference -v /home/dominika_kresa/mounted_from_Second/clubfoot_DK:/work smoove_ao
 bash lumpy_script.sh
+
+### Manta
+# Docker
+docker run -it -v /home/dominika_kresa/mounted_from_Second/nie_pacz:/work/my_data -v /home/dominika_kresa/reference/:/work/reference -v /home/dominika_kresa/mounted_from_Second/clubfoot_DK:/work smoove_ao
+bash manta_script.sh
